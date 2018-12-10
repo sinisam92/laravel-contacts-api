45b8ddc3c7986452c3e5c407a5ef30a4d43ca0ab
Sinisa Manojlovic <sinisamanojlovic1@gmail.com>
2018-12-10 21:31:11 +0100

    Commit conntacts api

---
+++ database/seeds/ContactsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Contact;
+class ContactsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       factory(Contact::class, 100)->create();
+    }
+}
