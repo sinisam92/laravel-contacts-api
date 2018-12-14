21d468095e5fb8a6102b63bc901b52686637bba6
Sinisa Manojlovic <sinisamanojlovic1@gmail.com>
2018-12-14 20:56:34 +0100

    zavrsen back

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 */
 // globalno dodaje hedere nije preporucljivo
 // header('Access-Control-Allow-Origin: *');
-// header('Access-Control-Allow-Methods: PUT, GET,POST,DELETE,OPTIONS');
+// header('Access-Control-Allow-Methods: PUT,GET,POST,DELETE,OPTIONS');
 // header('Access-Control-Allow-Headers: Content-Type, Accept,Origin');
 
 Route::group([
